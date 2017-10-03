169312a42c153ea9ff7df8adeb4c9ada0012d55f
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-10-04 01:09:10 +0300

    Add failing unit test

--- src/main/scala/markov/Interpreter.scala
+++ src/main/scala/markov/Interpreter.scala
@@ -1,9 +1,25 @@
 package markov
 
-case class Rule(from: String, to: String, isTerminal: Boolean)
+trait Rule {
+  def from: String
+  def to: String
+}
+
+case class ContinuingRule(from: String, to: String) extends Rule
+
+case class TerminatingRule(from: String, to: String) extends Rule
+
 case class Algorithm(rules: List[Rule])
-case class EvaluationStep(rule: Rule, before: String, after: String)
-case class Evaluation(steps: List[EvaluationStep])
+
+case class EvaluationStep(before: String, after: String, rule: Rule)
+
+case class Evaluation(steps: List[EvaluationStep]) {
+  def result(): Option[String] =
+    if (steps.size > 0)
+      Some(steps.last.after)
+    else
+      None
+}
 
 object Interpreter {
 
---
+++ src/test/scala/markov/InterpreterSpec.scala
@@ -0,0 +1,43 @@
+package markov
+
+import collection.mutable.Stack
+import org.scalatest._
+
+class InterpreterSpec extends FunSuite {
+
+  test("evaluate simple algorithm") {
+
+    //#Changes ones to zeros
+    //q1>0q
+    //q0>0q
+    //q>.
+    //>q
+    val changeOneToZeroRule = ContinuingRule("q1", "0q")
+    val leaveZeroInPlaceRule = ContinuingRule("q0", "0q")
+    val finishAlgorithmRule = TerminatingRule("q", "")
+    val startAlgorithmRule = ContinuingRule("", "q")
+    val changeOnesToZeros = Algorithm(List(
+      changeOneToZeroRule,
+      leaveZeroInPlaceRule,
+      finishAlgorithmRule,
+      startAlgorithmRule
+    ))
+
+    val input = "111"
+    val evaluation = Interpreter.execute(changeOnesToZeros, input)
+
+    val expectedEvaluation = Evaluation(List(
+      EvaluationStep("111", "q111", startAlgorithmRule),
+      EvaluationStep("q111", "0q11", changeOneToZeroRule),
+      EvaluationStep("0q11", "00q1", changeOneToZeroRule),
+      EvaluationStep("00q1", "000q", changeOneToZeroRule),
+      EvaluationStep("000q", "000", finishAlgorithmRule),
+    ))
+
+    assert(evaluation == expectedEvaluation)
+  }
+
+  //TODO: Order or rules should matter, rules are applied in the order of definition
+  //TODO: No rules provided
+  //TODO: What happens with a non-terminating algorithm?
+}
