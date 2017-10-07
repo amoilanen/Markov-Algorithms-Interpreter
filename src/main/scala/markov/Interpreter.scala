11d0906e2495b871a8c964e1c86bfbe937ded625
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-10-08 00:41:12 +0300

    Extract rule application function into the Rule trait

--- src/main/scala/markov/Interpreter.scala
+++ src/main/scala/markov/Interpreter.scala
@@ -5,6 +5,11 @@ import scala.annotation.tailrec
 trait Rule {
   def from: String
   def to: String
+  def apply(inputBefore: String): RuleEvaluation = {
+    val inputAfter = inputBefore.replaceFirst(from, to)
+
+    RuleEvaluation(inputBefore, inputAfter, this)
+  }
 }
 
 case class ContinuingRule(from: String, to: String) extends Rule
@@ -13,9 +18,9 @@ case class TerminatingRule(from: String, to: String) extends Rule
 
 case class Algorithm(rules: List[Rule])
 
-case class EvaluationStep(before: String, after: String, rule: Rule)
+case class RuleEvaluation(before: String, after: String, rule: Rule)
 
-case class Evaluation(steps: List[EvaluationStep]) {
+case class AlgorithmEvaluation(steps: List[RuleEvaluation]) {
   def result(): Option[String] =
     if (steps.size > 0)
       Some(steps.last.after)
@@ -25,23 +30,23 @@ case class Evaluation(steps: List[EvaluationStep]) {
 
 object Interpreter {
 
-  def execute(algorithm: Algorithm, input: String): Evaluation = {
+  def execute(algorithm: Algorithm, input: String): AlgorithmEvaluation = {
 
     @tailrec
-    def executeNextRules(algorithm: Algorithm, input: String, steps: List[EvaluationStep]): Evaluation = {
+    def executeNextRules(algorithm: Algorithm, input: String, steps: List[RuleEvaluation]): AlgorithmEvaluation = {
       val nextApplicableRule = algorithm.rules.find(rule => input.contains(rule.from))
       nextApplicableRule match {
         case Some(rule) => {
-          val inputAfterRuleApplication = input.replaceFirst(rule.from, rule.to)
-          val step = EvaluationStep(input, inputAfterRuleApplication, rule)
+          val step = rule.apply(input)
+          val inputAfterRuleApplication = step.after
           val updatedSteps = steps :+ step
 
           rule match {
-            case TerminatingRule(_, _) => Evaluation(updatedSteps)
+            case TerminatingRule(_, _) => AlgorithmEvaluation(updatedSteps)
             case ContinuingRule(_, _) => executeNextRules(algorithm, inputAfterRuleApplication, updatedSteps)
           }
         }
-        case None => Evaluation(steps)
+        case None => AlgorithmEvaluation(steps)
       }
     }
 
--- src/test/scala/markov/InterpreterSuite.scala
+++ src/test/scala/markov/InterpreterSuite.scala
@@ -25,19 +25,26 @@ class InterpreterSpec extends FunSuite {
     val input = "111"
     val evaluation = Interpreter.execute(changeOnesToZeros, input)
 
-    val expectedEvaluation = Evaluation(List(
-      EvaluationStep("111", "q111", startAlgorithmRule),
-      EvaluationStep("q111", "0q11", changeOneToZeroRule),
-      EvaluationStep("0q11", "00q1", changeOneToZeroRule),
-      EvaluationStep("00q1", "000q", changeOneToZeroRule),
-      EvaluationStep("000q", "000", finishAlgorithmRule),
+    val expectedEvaluation = AlgorithmEvaluation(List(
+      RuleEvaluation("111", "q111", startAlgorithmRule),
+      RuleEvaluation("q111", "0q11", changeOneToZeroRule),
+      RuleEvaluation("0q11", "00q1", changeOneToZeroRule),
+      RuleEvaluation("00q1", "000q", changeOneToZeroRule),
+      RuleEvaluation("000q", "000", finishAlgorithmRule),
     ))
 
     assert(evaluation == expectedEvaluation)
   }
 
+  //TODO: Test that . is interpreted as a dot symbol, not as any symbol
   //TODO: Order or rules should matter, rules are applied in the order of definition
   //TODO: Only one and the first occurrence of the rule's left part is substituted
   //TODO: No rules provided
   //TODO: What happens with a non-terminating algorithm?
+  //TODO: '>' is not an allowed symbol
+
+  //TODO: REPL mode, algorithm is input as alg="q1>0q >> q0>0q >> q>. >> >q"
+  //Then REPL prints alg=Algorithm(...)
+  //Then alg 111 would print all the evaluation steps and the final result
+  //TODO: File evaluation mode: will load an algorithm in the provided file and apply it against the provided input
 }
